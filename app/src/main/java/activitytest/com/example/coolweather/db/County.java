2a3c09a43759e7ed3234c93ea0d7d6bae7fde040
CSZ <1312463385@qq.com>
2017-02-07 14:01:35 +0800

    完成遍历省市县三级列表的功能。。.

--- app/src/main/java/activitytest/com/example/coolweather/db/County.java
+++ app/src/main/java/activitytest/com/example/coolweather/db/County.java
@@ -34,5 +34,15 @@ public class County extends DataSupport {
     {
         this.weatherId = weatherId;
     }
+    public int getCityId()
+    {
+        return cityId;
+    }
+    public void setCityId(int cityId)
+    {
+        this.cityId = cityId;
+    }
+
+
 
 }
