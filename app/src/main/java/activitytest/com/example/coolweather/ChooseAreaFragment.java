93cd2bcd16c26a6ea92e94c260c8821c6c2cfd41
CSZ <1312463385@qq.com>
2017-02-08 17:35:42 +0800

    新增切换城市和手动更新天气的功能。

--- app/src/main/java/activitytest/com/example/coolweather/ChooseAreaFragment.java
+++ app/src/main/java/activitytest/com/example/coolweather/ChooseAreaFragment.java
@@ -84,20 +84,26 @@ public class ChooseAreaFragment extends Fragment {
         super.onActivityCreated(savedInstanceState);
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
             @Override
-            public void onItemClick(AdapterView<?> parent,View view,int position, long id)
-            {
-                if (currentLevel == LEVEL_PROVINCE){
+            public void onItemClick(AdapterView<?> parent,View view,int position, long id) {
+                if (currentLevel == LEVEL_PROVINCE) {
                     selectedProvince = provinceList.get(position);
                     queryCities();
-                } else if(currentLevel == LEVEL_CITY){
+                } else if (currentLevel == LEVEL_CITY) {
                     selectedCity = cityList.get(position);
                     queryCounties();
-                } else  if(currentLevel == LEVEL＿COUNTY){
+                } else if (currentLevel == LEVEL＿COUNTY) {
                     String weatherId = countyList.get(position).getWeatherId();
-                    Intent intent = new Intent(getActivity(),WeatherActivity.class);
-                    intent.putExtra("weather_id",weatherId);
-                    startActivity(intent);
-                    getActivity().finish();
+                    if (getActivity() instanceof MainActivity) {
+                        Intent intent = new Intent(getActivity(), WeatherActivity.class);
+                        intent.putExtra("weather_id", weatherId);
+                        startActivity(intent);
+                        getActivity().finish();
+                    } else if (getActivity() instanceof WeatherActivity) {
+                        WeatherActivity activity = (WeatherActivity) getActivity();
+                        activity.drawerLayout.closeDrawers();
+                        activity.swipeRefresh.setRefreshing(true);
+                        activity.requestWeather(weatherId);
+                    }
                 }
             }
 
